7282cab1ca3e837f162d6f82d172637f40a6d1e4
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-06-09 15:34:09 +0200

    First Commit

--- laravel_molisana/webpack.mix.js
+++ laravel_molisana/webpack.mix.js
@@ -12,5 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .copyDirectory('resources/img', 'public/img')
     .sass('resources/sass/app.scss', 'public/css')
-        .sass('resources/sass/aboutme.scss', 'public/css');
+    .sass('resources/sass/aboutme.scss', 'public/css')
+    .options({
+        processCssUrl: false
+    });
